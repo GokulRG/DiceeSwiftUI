e855677be2cc7beb6a9f7d671c7fa8abdb565a6f
GokulRG <ramakrishnan.go@husky.neu.edu>
2021-03-04 00:21:31 -0800

    Dicee project completed in SwiftUI

---
+++ DiceySwiftUI/ContentView.swift
@@ -0,0 +1,55 @@
+//
+//  ContentView.swift
+//  DiceySwiftUI
+//
+//  Created by Gokul Rama on 3/3/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State var leftDiceNumber = 1
+    @State var rightDiceNumber = 1
+    
+    var body: some View {
+        ZStack {
+            Image("background").resizable().edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            VStack {
+                Image("diceeLogo")
+                Spacer()
+                HStack {
+                    DiceView(imageNumber: leftDiceNumber)
+                    DiceView(imageNumber: rightDiceNumber)
+                }.padding(.horizontal)
+                Spacer()
+                Button(action: {
+                    self.leftDiceNumber = Int.random(in: 1...6)
+                    self.rightDiceNumber = Int.random(in: 1...6)
+                }) {
+                    Text("Roll")
+                        .font(.system(size: 50))
+                        .fontWeight(.heavy)
+                        .foregroundColor(.white)
+                        .padding(.horizontal)
+                }.background(Color.red)
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+struct DiceView: View {
+    
+    let imageNumber: Int
+    var body: some View {
+        Image("dice\(imageNumber)")
+            .resizable().aspectRatio(1, contentMode: .fit)
+            .padding()
+    }
+}
